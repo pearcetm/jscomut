42145d71d50666d70007facc737a01b7f0f19e4f
Tom <tmpearce@gmail.com>
2018-02-23 09:21:27 -0500

    updated demo

--- jscomutdemo.js
+++ jscomutdemo.js
@@ -9,9 +9,29 @@ $(document).ready(function(){
     $('<button>', { class: 'btn btn-default' }).text('Sort Both').on('click', function () { comut.sort(); }).appendTo('#test-buttons');
     $('button').css({ margin: '5px' });
     var exampleData = $('<div>').prependTo($('.load-data')).css({padding:'5px',border:'thin gray solid'});
-    $('<div>').text('This demonstration contains example data with a small number of samples, and includes both genomic and demographic data.').appendTo(exampleData);
+    $('<div>').html('This demonstration contains example data with a small number of samples, and includes both genomic and demographic data.').appendTo(exampleData);
     $('<button>').text('Click here to load example data').on('click',function(){
-            comut.setData($('#example-mutations').text(), $('#example-annotations').text(),'Demonstration data');
+    			$.ajax({
+    				type:'GET',
+    				url:'glio_variants.txt',
+    				success:function(d,j,x){
+    					comut.setData(d,null,'Example variant data');
+    				},
+					error:function(d){
+						console.log('Error loading glio_variants.txt:', d);
+					}    				
+    			});
+    			$.ajax({
+    				type:'GET',
+    				url:'glio_demog.txt',
+    				success:function(d,j,x){
+    					comut.setData(null,d,'Example demographic data');
+    				},
+					error:function(d){
+						console.log('Error loading glio_demog.txt:', d);
+					}    				
+    			});
+            
         }).appendTo(exampleData).css('margin-top','5px');
     
 });
